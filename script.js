d57689ebd51517e5a3e9b41f5e1b4d39c6ba87e1
よしまる <bassara1034@gmail.com>
2025-07-31 18:28:37 +0900

    ZenspaUsuki自己写真以外完成

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-// JavaScript for interactive elements will go here.
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // 3秒ごとに画像を切り替える
+}
+
+// Modal
+document.addEventListener('DOMContentLoaded', function() {
+    var modal = document.getElementById('myModal');
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    var menuImages = document.querySelectorAll('.menu-item img');
+
+    menuImages.forEach(function(img) {
+        img.onclick = function(){
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+    });
+
+    var span = document.getElementsByClassName("close")[0];
+
+    span.onclick = function() { 
+        modal.style.display = "none";
+    }
+});
